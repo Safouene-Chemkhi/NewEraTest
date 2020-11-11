29f100b8bf535e308adac65dcab6511778bfa9c9
Safouene Chemkhi <safouene.chemkhi@supcom.tn>
2020-11-11 11:08:51 -0500

    Second ex

--- NewEraTest.js
+++ NewEraTest.js
@@ -32,17 +32,30 @@ async function ex1() {
 
 
 /**
-  * @desc Finding the next higher number
-  * @param void $msg - Please type a number:
-  * @return void - success or failure
-  * @exampel if the given number is 1234, the next higher number with the same digits is 1243.
+  * @desc Shopping sytem that produces the total cost
+  * @param void $msg - Please type a list of products:(between each two items it you should insert ", ")
+  * @return void 
+  * @exampel Apple, Apple, Orange, Apple => 3.05 $
 */
 
-function ex2(params) {
-    
+async function ex2() {
+    io.write('Please type a list of products:');
+    var x = await io.read();
+    var list = x.split(", ");
+    price = {
+        'Apple' :0.6,
+        'Orange' :1.25
+    }
+    var total = 0;
+    for (let i = 0; i < list.length; i++) {
+        total += price[list[i]];
+    }
+
+    io.write(total.toFixed(2) + '$');
 }
 
 
 /*********************Main************** */
+ex1();
+ex2();
 
-ex1()
