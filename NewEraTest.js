ef13a7ee26285ac114d2df7933bc7d3d88efe3ef
Safouene Chemkhi <safouene.chemkhi@supcom.tn>
2020-11-11 13:03:10 -0500

    fixing bug

--- NewEraTest.js
+++ NewEraTest.js
@@ -2,6 +2,17 @@
 const io = require('console-read-write');
 const prompts = require('prompts');
 
+
+/**
+  * @desc replace a char by specifying a index
+  * @param1 number
+  * @param2 string
+  * @return string 
+*/
+String.prototype.swap = function (index, chr1, chr2) {
+  return this.substr(0, index) + chr2 + chr1 + this.substr(index + 2);
+}
+
 /**
   * @desc Finding the next higher number
   * @param void $msg - Please type a number:
@@ -10,24 +21,22 @@ const prompts = require('prompts');
 */
 
 async function ex1() {
-    io.write('Please type a number:');
-    var num = await io.read();
-    var  n = num.toString();
-    var found = false;
-    i=n.length-1;
-    while (!found  && i>0){
-         if(n[i]>n[i-1]){
-            found = true;
-             var x= n.charAt(i);
-             n = n.replace(n.charAt(i),n.charAt(i-1));
-             n = n.replace(n.charAt(i-1),x);
-             io.write(n);  
-         }
-         i--;
+  io.write('Please type a number:');
+  var num = await io.read();
+  var n = num.toString();
+  var found = false;
+  i = n.length - 1;
+  while (!found && i > 0) {
+    if (n[i] > n[i - 1]) {
+      found = true;
+      n = n.swap(i - 1, n[i - 1], n[i])
+      io.write(n)
     }
-    if (!found){
-        io.write('There is no such number!');
-    }
+    i--;
+  }
+  if (!found) {
+    io.write('There is no such number!');
+  }
 }
 
 
@@ -39,19 +48,19 @@ async function ex1() {
 */
 
 async function ex2() {
-    io.write('Please type a list of products:');
-    var x = await io.read();
-    var list = x.split(", ");
-    price = {
-        'Apple' :0.6,
-        'Orange' :1.25
-    }
-    var total = 0;
-    for (let i = 0; i < list.length; i++) {
-        total += price[list[i]];
-    }
+  io.write('Please type a list of products:');
+  var x = await io.read();
+  var list = x.split(", ");
+  price = {
+    'Apple': 0.6,
+    'Orange': 1.25
+  }
+  var total = 0;
+  for (let i = 0; i < list.length; i++) {
+    total += price[list[i]];
+  }
 
-    io.write(total.toFixed(2) + '$');
+  io.write(total.toFixed(2) + '$');
 }
 
 
@@ -62,23 +71,23 @@ async function ex2() {
 */
 
 async function main() {
-    const response = await prompts({
-        type: 'select',
-        name: 'value',
-        message: 'Pick a function to test',
-        choices: [
-          { title: 'Exerice 1', description: 'Finding next higher number', value: '1' },
-          { title: 'Exerice 2', description: 'Shopping system', value: '2' },
+  const response = await prompts({
+    type: 'select',
+    name: 'value',
+    message: 'Pick a function to test',
+    choices: [
+      { title: 'Exerice 1', description: 'Finding next higher number', value: '1' },
+      { title: 'Exerice 2', description: 'Shopping system', value: '2' },
 
-        ],
-        initial: 0
-      })
+    ],
+    initial: 0
+  })
 
-      if(response.value==='1'){
-          ex1();
-      }else{
-          ex3();
-      }
+  if (response.value === '1') {
+    ex1();
+  } else {
+    ex3();
+  }
 }
 
 
